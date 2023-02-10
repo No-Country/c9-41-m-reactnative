e765d9f6d836279a3b8d321f12b82e0605c8f3a4
lucianosls <luchosalas111@gmail.com>
2023-02-10 17:47:20 -0300

    probando eslint y libreria de icons

--- frontend-c9-41-m/src/components/Header.jsx
+++ frontend-c9-41-m/src/components/Header.jsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import Constans from 'expo-constants';
-import {Text, View, StyleSheet, Image} from 'react-native';
 
-
-
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 const Header = () => {
-    return (
-    <View style={styles.container} >
-        <Text style={styles.header}>Hola, Claudia.</Text>
-        <Image
-            source={require('../../assets/usuario.png')}
-            style ={styles.image}
-        />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Hola, Claudia.</Text>
+      <Image
+        source={require('../../assets/usuario.png')}
+        style={styles.image}
+      />
     </View>
-    )
-};
+  )
+}
 
 const styles = StyleSheet.create({
-    container:{flexDirection:'row',},
-    header: { marginTop:30,marginLeft: 5, fontWeight: 'bold', fontSize: 20},
-    image: {height:40, width:40, borderRadius:20, marginTop:30, marginLeft: 230, marginRight:5 },
+  container: { flexDirection: 'row' },
+  header: { marginTop: 30, marginLeft: 5, fontWeight: 'bold', fontSize: 20 },
+  image: { height: 40, width: 40, borderRadius: 20, marginTop: 30, marginLeft: 230, marginRight: 5 }
 })
 
-export default Header;
+export default Header
