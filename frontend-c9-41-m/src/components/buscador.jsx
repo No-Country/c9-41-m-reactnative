aca96e84ef955d08bd9d753e933076db6bccce03
Zoe Guzman <zoeguzman.ok@gmail.com>
2023-02-12 19:24:42 -0300

    buscador #2

--- frontend-c9-41-m/src/components/buscador.jsx
+++ frontend-c9-41-m/src/components/buscador.jsx
@@ -1,34 +1,52 @@
-
 import React from 'react'
-import { View, StyleSheet, TextInput } from 'react-native'
+import { View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 const Buscador = () => {
   const [text, onChangeText] = React.useState('Buscar')
 
   return (
-    <View>
+    <View style={styles.InputContainer}>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
-
       />
+      <TouchableOpacity
+        style={styles.button}
+      >
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          size={24}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   input: {
-
-    height: 40,
+    height: 45,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     backgroundColor: '#D9D9D9',
-    borderRadius: 20,
-    width: '85%',
+    borderRadius: 15,
+    width: '88%',
     alignSelf: 'center'
-
+  },
+  InputContainer: {
+    position: 'relative',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    position: 'absolute',
+    right: 35,
+    bottom: 16,
+    padding: 8
   }
 })
 
