0a124fee24b693d625cf64c6baa1d329682213a4
tomasherrera1910 <tomymza10@gmail.com>
2023-02-14 10:51:11 -0300

    add router

--- frontend-c9-41-m/App.js
+++ frontend-c9-41-m/App.js
@@ -1,7 +1,16 @@
-import { View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Login from './src/components/Login'
+import { Register } from './src/components/Register'
 
+const Stack = createNativeStackNavigator()
 export default function App () {
   return (
-    <View />
-    )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Register' component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
