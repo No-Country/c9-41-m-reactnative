d2ab4ee1f0e16c16a0cb767cdc3e5beb1eba3d29
Ray  Cardenas <raykevin1999@gmail.com>
2023-02-16 09:15:28 -0500

    arreglado el formik-yup

--- frontend-c9-41-m/App.js
+++ frontend-c9-41-m/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import LoginFormikYup from './src/components/LoginFormikYup'
+import Login from './src/components/Login'
 import { Register } from './src/components/Register'
 
 const Stack = createNativeStackNavigator()
@@ -8,9 +8,10 @@ export default function App () {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={LoginFormikYup} />
+        <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Register' component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
+
   )
 }
