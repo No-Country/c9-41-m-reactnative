d1b1cd4eea18fcb46264794c040316eafd8419a5
tomasherrera1910 <tomymza10@gmail.com>
2023-02-17 20:39:40 -0300

    Add navbar

--- frontend-c9-41-m/App.js
+++ frontend-c9-41-m/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Categories } from './src/components/Categories'
 import LoginFormikYup from './src/components/LoginFormikYup'
 import { Register } from './src/components/Register'
 
@@ -7,9 +8,10 @@ const Stack = createNativeStackNavigator()
 export default function App () {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Nuestra Carta'>
         <Stack.Screen name='Login' component={LoginFormikYup} />
         <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='Nuestra Carta' component={Categories} />
       </Stack.Navigator>
     </NavigationContainer>
   )
