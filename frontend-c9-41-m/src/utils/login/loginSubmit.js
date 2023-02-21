17e4409b5be7bd5e8223a22121455044424813a4
tomasherrera1910 <tomymza10@gmail.com>
2023-02-21 19:57:13 -0300

    handling unauthorized error

--- frontend-c9-41-m/src/utils/login/loginSubmit.js
+++ frontend-c9-41-m/src/utils/login/loginSubmit.js
@@ -9,10 +9,15 @@ export default async function loginSubmit (values) {
       credentials: 'include',
       body: JSON.stringify(parsedValues)
     })
+    if (response.status === 401) {
+      throw new Error('Email y/o contraseña incorrectos')
+    }
+    console.log(response.headers.get('set-cookie'))
     const json = await response.json()
     console.log({ json })
     return json
   } catch (e) {
     console.error(e)
+    return { error: e.message }
   }
 }
