791b67ee2fd91ec28f71cadcd49833ee859080a0
Ray  Cardenas <raykevin1999@gmail.com>
2023-02-22 18:07:01 -0500

    mediopago

--- frontend-c9-41-m/App.js
+++ frontend-c9-41-m/App.js
@@ -2,13 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Car from './src/components/Car'
 import Login from './src/components/Login'
+import MedioPago from './src/components/MedioPago'
 import Product from './src/components/Product'
 import { Register } from './src/components/Register'
 
 const Stack = createNativeStackNavigator()
 export default function App () {
   return (
-    <Car />
+    <MedioPago />
     // <NavigationContainer>
     //   <Stack.Navigator initialRouteName='Login'>
     //     <Stack.Screen name='Login' component={Login} />
