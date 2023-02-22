d42b5db0d883fdab66c0a1ddb0b36d04a22bea54
Ray  Cardenas <raykevin1999@gmail.com>
2023-02-22 11:44:13 -0500

    Cart

--- frontend-c9-41-m/App.js
+++ frontend-c9-41-m/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Car from './src/components/Car'
 import Login from './src/components/Login'
 import Product from './src/components/Product'
 import { Register } from './src/components/Register'
@@ -7,13 +8,13 @@ import { Register } from './src/components/Register'
 const Stack = createNativeStackNavigator()
 export default function App () {
   return (
+    <Car />
     // <NavigationContainer>
     //   <Stack.Navigator initialRouteName='Login'>
     //     <Stack.Screen name='Login' component={Login} />
     //     <Stack.Screen name='Register' component={Register} />
     //   </Stack.Navigator>
     // </NavigationContainer>
-    <Product />
 
   )
 }
