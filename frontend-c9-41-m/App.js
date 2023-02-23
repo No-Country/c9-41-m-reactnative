7cc9334720109e67fd2bc1f08faca75e488db6c7
tomasherrera1910 <tomymza10@gmail.com>
2023-02-22 22:44:09 -0300

    App with login and home

--- frontend-c9-41-m/App.js
+++ frontend-c9-41-m/App.js
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Login from './src/components/Login'
 import { Register } from './src/components/Register'
 import Home from './src/components/Home/Home'
-import Categories from './src/components/Categories/Index'
+import Categories from './src/components/Categories'
 import MedioDePago from './src/components/MedioDePago'
 import { Favorites } from './src/components/Favorites/Favorites'
 
@@ -11,16 +11,15 @@ const Stack = createNativeStackNavigator()
 export default function App () {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Register' component={Register} />
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='Categories' component={Categories} />
         <Stack.Screen name='MediodePago' component={MedioDePago} />
         <Stack.Screen name='Favorites' component={Favorites} />
-       </Stack.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
 // Exportar todas los componentes de la misma forma, se recomienda export default porque en los archivos solo se exporta una sola función, que es el componente
-
