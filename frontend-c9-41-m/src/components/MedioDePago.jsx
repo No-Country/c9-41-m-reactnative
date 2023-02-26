fcc7489f55af6ddfdfde2474120d7fb480af6353
lucianosls <luchosalas111@gmail.com>
2023-02-26 12:38:54 -0300

    avances en vista medio de pago

--- frontend-c9-41-m/src/components/MedioDePago.jsx
+++ frontend-c9-41-m/src/components/MedioDePago.jsx
@@ -5,69 +5,70 @@ import { faArrowLeft, faCircle, faCreditCard } from '@fortawesome/free-solid-svg
 const MedioDePago = () => {
   return (
     <View>
-      <FontAwesomeIcon style={styles.icon} icon={faArrowLeft} size={24} />
+      <View style={styles.flecha}>
+        <FontAwesomeIcon style={styles.icon} icon={faArrowLeft} size={24} />
+      </View>
       <Text style={styles.title}> Elige medio de pago </Text>
-      <View style={styles.pagos}>
-        <View style={styles.line}>
-          <Text style={styles.tipoPago}>
-            <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
-            Agregar Tarjeta
-            <FontAwesomeIcon style={styles.icon} icon={faCreditCard} size={15} />
-          </Text>
+      <View style={styles.medios}>
+        <View style={styles.tipoPago}>
+          <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
+          <Text style={styles.text}> Mercado Pago </Text>
+          <FontAwesomeIcon style={styles.icon} icon={faCreditCard} size={15} />
         </View>
-        <View style={styles.line}>
-          <Text style={styles.tipoPago}>
-            <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
-            Efectivo
-          </Text>
+        <View style={styles.tipoPago}>
+          <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
+          <Text style={styles.text}> Efectivo </Text>
         </View>
-        <View style={styles.line}>
-          <Text style={styles.tipoPago}>
-            <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
-            Retiro en el Local
-          </Text>
+        <View style={styles.tipoPago}>
+          <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
+          <Text style={styles.text}> Retiro en el Local </Text>
         </View>
       </View>
-      <View style={styles.containerTotal}>
-        <Text style={styles.total}>Total a pagar                                      $1800</Text>
-      </View>
-      <View>
-        <Text styel={styles.añadir}>Añadir</Text>
+      <View style={styles.total}>
+        <Text style={styles.precio}> Total a Pagar</Text>
+        <Text style={styles.precio}> $1800</Text>
       </View>
     </View>
 
   )
 }
 const styles = StyleSheet.create({
-  icon: {
-    marginTop: 45,
-    marginLeft: 15
-
+  flecha: {
+    marginLeft: 16,
+    marginTop: 32
   },
   title: {
     textAlign: 'center',
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 70
   },
+
   tipoPago: {
-    fontSize: 17,
-    marginBottom: 20
+    flexDirection: 'row',
+    margin: 52,
+    marginLeft: 18,
+    marginTop: 18,
+    borderBottomWidth: 2,
+    borderBottomColor: '#FB6D3B'
   },
-  line: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 20
+  medios: {
+    marginLeft: 5,
+    marginRight: 5
   },
-  pagos: {
-    marginTop: 50
+  text: {
+    marginBottom: 18
   },
   total: {
-    fontSize: 17,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    flexDirection: 'row',
+    marginTop: 120,
+    marginLeft: 18
+  },
+  precio: {
+    fontSize: 16,
+    fontWeight: 'bold'
   }
+
 }
 
 )
