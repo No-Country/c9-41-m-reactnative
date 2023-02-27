c448614ca84eaa55737bdaf934beb7688a8ffffa
lucianosls <luchosalas111@gmail.com>
2023-02-27 15:22:24 -0300

    avances en metodo de pago

--- frontend-c9-41-m/src/components/MedioDePago.jsx
+++ frontend-c9-41-m/src/components/MedioDePago.jsx
@@ -1,38 +1,54 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faArrowLeft, faCircle, faCreditCard } from '@fortawesome/free-solid-svg-icons'
 
 const MedioDePago = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.flecha}>
-        <FontAwesomeIcon style={styles.icon} icon={faArrowLeft} size={24} />
+        <TouchableOpacity>
+          <FontAwesomeIcon style={styles.icon} icon={faArrowLeft} size={24} />
+        </TouchableOpacity>
       </View>
       <Text style={styles.title}> Elige medio de pago </Text>
       <View style={styles.medios}>
         <View style={styles.tipoPago}>
-          <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
-          <Text style={styles.text}> Mercado Pago </Text>
-          <FontAwesomeIcon style={styles.icon} icon={faCreditCard} size={15} />
+          <TouchableOpacity style={styles.buttonTipoPago}>
+           <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
+            <Text style={styles.text}> Mercado Pago </Text>
+           <FontAwesomeIcon style={styles.icon} icon={faCreditCard} size={15} />
+          </TouchableOpacity>
         </View>
         <View style={styles.tipoPago}>
-          <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
-          <Text style={styles.text}> Efectivo </Text>
+          <TouchableOpacity style={styles.buttonTipoPago}>
+            <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
+            <Text style={styles.text}> Efectivo </Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.tipoPago}>
-          <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
-          <Text style={styles.text}> Retiro en el Local </Text>
+          <TouchableOpacity style={styles.buttonTipoPago}>
+            <FontAwesomeIcon style={styles.icon} icon={faCircle} size={15} />
+            <Text style={styles.text}> Retiro en el Local </Text>
+          </TouchableOpacity>
         </View>
       </View>
       <View style={styles.total}>
         <Text style={styles.precio}> Total a Pagar</Text>
-        <Text style={styles.precio}> $1800</Text>
+        <Text style={styles.valor}> $1800</Text>
+      </View>
+      <View  style = {styles.confirmarContainer}>
+        <TouchableOpacity style={styles.confirmarButton}>
+         <Text style={styles.confirmarText}> Confirmar </Text>
+        </TouchableOpacity>
       </View>
     </View>
 
   )
 }
 const styles = StyleSheet.create({
+buttonTipoPago:{
+  flexDirection:'row'
+},
   flecha: {
     marginLeft: 16,
     marginTop: 32
@@ -60,16 +76,39 @@ const styles = StyleSheet.create({
     marginBottom: 18
   },
   total: {
+    
     flexDirection: 'row',
     marginTop: 120,
     marginLeft: 18
   },
   precio: {
-    fontSize: 16,
-    fontWeight: 'bold'
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight:180
+  },
+  valor:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight:18
+  },
+  confirmarContainer:{
+  alignItems:'center',
+  marginTop:50
+  },  
+confirmarButton:{
+  borderColor: 'gray',
+    width: 328,
+    height: 45,
+    marginTop: 16,
+    borderRadius: 20,
+    backgroundColor: '#FB6D3B',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  confirmarText:{
+    color:'#FFFFFF'
   }
-
-}
+},
 
 )
 
