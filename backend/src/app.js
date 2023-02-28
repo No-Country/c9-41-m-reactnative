69430613d8fd589b3a21f4ab06bc1dcf5344dadf
Juan Cruz Musso <developjcm@gmail.com>
2023-02-28 18:48:49 +0100

    req.cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -79,7 +79,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   console.log("req.user", req.user);
   console.log("req.session.user", req.session);
-
+  console.log("req.cookies", req.cookies);
   next();
 });
 app.use("/auth", authRoutes);
@@ -90,20 +90,20 @@ app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 app.use("/payment", paymentRouter);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    `${process.env.PATH_FRONT}`,
-    `${process.env.PATH_FRONT_ADMIN}`,
-    `http://localhost:3000`,
-  ]);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", [
+//     `${process.env.PATH_FRONT}`,
+//     `${process.env.PATH_FRONT_ADMIN}`,
+//     `http://localhost:3000`,
+//   ]);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // Manejo errores
 app.use((err, req, res, next) => {
