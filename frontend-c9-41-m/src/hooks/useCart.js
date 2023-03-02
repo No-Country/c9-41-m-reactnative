f461685b2a4d0378f9b5777257a965d6093bcee2
tomasherrera1910 <tomymza10@gmail.com>
2023-03-02 10:26:03 -0300

    add loading and function for update cart state after a delete

--- frontend-c9-41-m/src/hooks/useCart.js
+++ frontend-c9-41-m/src/hooks/useCart.js
@@ -3,13 +3,32 @@ import { getCart } from '../utils/user/cart'
 
 const useCart = () => {
   const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(null)
+  const [loading, isLoading] = useState(false)
   useEffect(() => {
+    isLoading(true)
     getCart()
       .then((data) => {
         setCart(data.cart)
+        setTotal(data.cart.reduce((acc, curr) => {
+          const subtotal = curr.productId.price * curr.quantity
+          return acc + subtotal
+        }, 0))
       })
+      .finally(() => { isLoading(false) })
   }, [])
-  return { cart }
+  const updateCart = (isLoading) => {
+    getCart()
+      .then((data) => {
+        setCart(data.cart)
+        setTotal(data.cart.reduce((acc, curr) => {
+          const subtotal = curr.productId.price * curr.quantity
+          return acc + subtotal
+        }, 0))
+      })
+      .finally(() => { isLoading(false) })
+  }
+  return { cart, total, updateCart, loading }
 }
 
 export default useCart
